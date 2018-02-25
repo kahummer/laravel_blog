01392d5d59aa26645cacdf016aa2e6904ccaa23b
Pharis Mukui <kahama94@gmail.com>
2018-02-25 16:56:32 +0300

    Users view plus create without photos

--- app/Http/Controllers/AdminUsersController.php
+++ app/Http/Controllers/AdminUsersController.php
@@ -3,6 +3,11 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Requests\UsersRequest;
+use App\User;
+use App\Role;
+
+
 
 class AdminUsersController extends Controller
 {
@@ -13,7 +18,9 @@ class AdminUsersController extends Controller
      */
     public function index()
     {
-        return view('admin.users.index');
+        $users = User::all();
+        return view('admin.users.index', compact('users'));
+        
     }
 
     /**
@@ -24,6 +31,10 @@ class AdminUsersController extends Controller
     public function create()
     {
         //
+        $roles = Role::pluck('name','id')->all();
+        
+
+        return view('admin.users.create', compact('roles'));
     }
 
     /**
@@ -32,9 +43,11 @@ class AdminUsersController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(UsersRequest $request)
     {
         //
+        User::create($request->all());
+        return redirect('/admin/users');
     }
 
     /**
--- resources/views/admin/users/create.blade.php
+++ resources/views/admin/users/create.blade.php
@@ -1,1 +1,53 @@
-create.blade.php
+@extends('layouts.admin')
+
+
+@section('content')
+
+
+<h1>Create Users</h1>
+
+{!! Form::open(['method' => 'Post', 'action'=>'AdminUsersController@store', 'files'=>'true']) !!}
+
+<div class="form-group"> 
+      {!! Form::label('name', 'Name')!!}
+      {!! Form::text('name', null, ['class'=>'form-control'])!!}
+</div>
+<div class="form-group"> 
+      {!! Form::label('email', 'Email')!!}
+      {!! Form::email('email', null, ['class'=>'form-control'])!!}
+</div>
+
+<div class="form-group"> 
+      {!! Form::label('role_id', 'Role_Id')!!}
+      {!! Form::select('role_id', [''=>'Choose Options'] + $roles, null, ['class'=>'form-control'])!!}
+</div>
+
+
+
+<div class="form-group"> 
+      {!! Form::label('is_active', 'Status')!!}
+      {!! Form::select('is_active', array(1=>'Active', 0=>'Not Active'), 0, ['class'=>'form-control'])!!}
+</div>
+
+<div class="form-group"> 
+      {!! Form::label('file', 'Upload Photo')!!}
+      {!! Form::file('file', null, ['class'=>'form-control'])!!}
+</div>
+
+
+
+<div class="form-group"> 
+      {!! Form::label('password', 'Password')!!}
+      {!! Form::password('password', ['class'=>'form-control'])!!}
+</div>
+
+<div class="form-group">
+	{!!Form::submit('Create User', ['class'=>'btn btn-primary'])!!}
+</div>
+
+
+{!! Form::close() !!}
+
+@include('includes.formerror');
+
+@stop
